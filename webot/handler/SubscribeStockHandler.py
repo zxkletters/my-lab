f5c7de8e9bb6344e19b86064c55283f8cd38c7db
xiaoke.zhangxk <xiaoke.zhangxk@alibaba-inc.com>
2014-09-22 16:28:33 +0800

    根据stockType进行不同的输出

--- webot/handler/SubscribeStockHandler.py
+++ webot/handler/SubscribeStockHandler.py
@@ -9,9 +9,11 @@ Created on 2014-9-22
 '''
 
 import httplib
+from operator import eq
 import time
 
 from Message import textTemplate
+import Message
 from utils import logInfo
 
 
@@ -29,23 +31,23 @@ class SubscribeStockHandler(object):
             qryCategory, _ , values = message.content.partition(":")
             helpInfos = "订阅股票: sbgp:[美股|A股] [股票代码] 100,如: sbgp:m baba 100"
             
-            if qryCategory != "sbgp" or qryCategory != "SBGP":
-                return textTemplate % (message.fromUserName, message.toUserName,
-                                   time.time(), helpInfos, 0)
             if not _ or not values:
                 # reply help infos
                 return textTemplate % (message.fromUserName, message.toUserName,
                                    time.time(), helpInfos, 0)
+            if qryCategory == "sbgp" or qryCategory == "SBGP":
+                sbValues = values.split(" ")
+                if len(sbValues) != 3:
+                    # reply help infos
+                    return textTemplate % (message.fromUserName, message.toUserName,
+                                       time.time(), helpInfos, 0)
                 
-            sbValues = values.split(" ")
-            if len(sbValues) != 3:
-                # reply help infos
+                qyrResult = self.batchFetchStockInfos([ x.rstrip().lstrip() for x in sbValues ])
+                return textTemplate % (message.fromUserName, message.toUserName,
+                                       time.time(), qyrResult, 0)
+            else:
                 return textTemplate % (message.fromUserName, message.toUserName,
                                    time.time(), helpInfos, 0)
-            
-            qyrResult = self.batchFetchStockInfos([ x.rstrip().lstrip() for x in sbValues ])
-            return textTemplate % (message.fromUserName, message.toUserName,
-                                   time.time(), qyrResult, 0)
         else:
             return None
     
@@ -78,17 +80,21 @@ class SubscribeStockHandler(object):
                     continue
                 
                 if v and len(v) > 0:
-                    finfo = self.printStockInfos(v)
+                    finfo = self.printStockInfos(v, stockType)
                     if finfo:
                         result.append(finfo)
             return "\n\n".join(result)
         else:
             print "query error! status =", response.status, ";", response.reason
 
-    def printStockInfos(self, stockInfo):
+    def printStockInfos(self, stockInfo, stockType):
         infos = stockInfo.split(",")
         if not infos:
             return ""
         else:
-            return u"%s:\n日期:%s\n时间:%s\n最新价:%s\n今开:%s\n最高:%s\n最低:%s\n昨收:%s\n涨跌幅:%s%s" \
-                % (infos[0].replace("\"", ""), infos[-3], infos[-2], infos[3], infos[1], infos[4], infos[5], infos[2], round(((float(infos[3]) - float(infos[2])) / float(infos[2])) * 100, 2), "%")
+            if stockType == "m":
+                return u"%s:\n时间:%s\n最新价:%s\n涨跌:%s\n涨跌幅:%s%s" \
+                    % (infos[0].replace("\"", ""), infos[-3], infos[1], infos[4], infos[2], "%")
+            else:
+                return u"%s:\n日期:%s\n时间:%s\n最新价:%s\n今开:%s\n最高:%s\n最低:%s\n昨收:%s\n涨跌幅:%s%s" \
+                    % (infos[0].replace("\"", ""), infos[-3], infos[-2], infos[3], infos[1], infos[4], infos[5], infos[2], round(((float(infos[3]) - float(infos[2])) / float(infos[2])) * 100, 2), "%")
